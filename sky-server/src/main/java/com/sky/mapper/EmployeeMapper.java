9b47a0d9aab8e20d36c93eec568dad672b53dc03
littlebear001 <2683933368@qq.com>
2024-01-09 15:47:35 +0800

    编辑员工信息

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -39,4 +39,12 @@ public interface EmployeeMapper {
      * @param build
      */
     void update(Employee build);
+
+    /**
+     * 根据id查询员工
+     * @param id
+     * @return
+     */
+    @Select("select *from employee where id = #{id}")
+    Employee getById(Long id);
 }
